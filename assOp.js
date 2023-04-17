5f754abedda8c5562ff48dd47b19375bc1428b59
Joseph Kioko <kiokocodes@gmail.com>
2023-04-18 02:25:20 +0300

    initial commit for the assignement operator

---
+++ assOp.js
@@ -0,0 +1,51 @@
+
+// Create a variable called x and set its initial value to 5 -->
+
+    let x = 5;
+	
+
+// Add 3 to the value of x using the + operator, and assign the new value (8) back to x -->
+	
+    x = x + 3;
+
+
+// Add 2 to the value of x using the += operator, and assign the new value (10) back to x -->
+	
+    x += 2;
+
+    // Appends output value of x to the HTML span element
+    document.getElementById("output").innerHTML = x;
+
+
+
+
+
+
+
+
+
+//DETAILED EXPLANATIONS BELOW ARE FOR YOUTUBE TUTORIAL
+
+
+/* 
+
+let x = 5;  // the value 5 is assigned to the variable x using the equals sign (=) assignment operator
+console.log(x);  // output: 5
+
+x = x + 3;  // the value of x is added to 3 using the plus sign (+) arithmetic operator, then the result is assigned back to x using the equals sign (=) assignment operator
+console.log(x);  // output: 8
+
+x += 2;  // the value of x is added to 2 using the plus sign (+) arithmetic operator, then the result is assigned back to x using the compound assignment operator (+=)
+console.log(x);  // output: 10
+
+*/
+
+
+
+/*
+let y = "hello";  // the string "hello" is assigned to the variable y using the equals sign (=) assignment operator
+console.log(y);  // output: "hello"
+
+y = y + " world";  // the string " world" is concatenated to the string in y using the plus sign (+) concatenation operator, then the result is assigned back to y using the equals sign (=) assignment operator
+console.log(y);  // output: "hello world"
+*/
